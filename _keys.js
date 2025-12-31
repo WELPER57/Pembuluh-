7a05ad2abed85097dd73cd7004731af845c08136
WELPER57 <dikagacor135@gmail.com>
2025-12-31 14:58:18 +0700

    Add files via upload

--- _keys.js
+++ _keys.js
@@ -1,14 +1,14 @@
 export const KEYS = {
   pakasir: {
-    project: "welper-official",
-    apiKey: "iVxbR4zOJgsJVEHazb04Q0SFVUyrsdxT",
+    project: "",
+    apiKey: "",
     baseUrl: "https://app.pakasir.com/api",
   },
 
   pterodactyl: {
-    domain: "https://www.sever.welperoffcial.my.id",
-    apiKey: "ptla_RV8QGuTlShDPrixWcXx5MWAfTeF2dmtpzqTeIuuNlpYi",
-    clientKey: "ptlc_xGM7FfXcfPfeUqQmT37R2etht8Gygh3BcnOA4qOL2pj",
+    domain: "https://",
+    apiKey: "",
+    clientKey: "",
     egg: 15,
     nestId: 5,
     locationId: 1,
